68fd0f53172b39289c12443d906379845959efdc
Lovjeet1233 <lovjeet_s@me.iitr.ac.in>
2025-01-29 02:16:48 +0530

    first commit

--- Functions/1.js
+++ Functions/1.js
@@ -3,3 +3,30 @@ function greet(name) {
     console.log("hello ".concat(name));
 }
 greet("Lovjeet singh");
+var names = ["alice", "bob", "jack"];
+names.forEach(function (s) {
+    console.log(s);
+});
+function Cordi(pt) {
+    console.log("x cordinate is ".concat(pt.x, " and y cordinate is ").concat(pt.y));
+}
+Cordi({ x: 7, y: 8 });
+function Cordii(pt) {
+    console.log("x cordinate is ".concat(pt.x, " and y cordinate is ").concat(pt.y));
+}
+Cordii({ x: 7, y: 8 });
+// Defining the `getStudent` function
+function getStudent() {
+    return {
+        name: "John Doe",
+        course: "Mathematics",
+        marks: 95
+    };
+}
+// Using the function
+var detail = getStudent();
+console.log(detail.name);
+var req = { url: "https://example.com", method: "GET" };
+// Change 2
+handleRequest(req.url, req.method);
+console.log(handleRequest);
--- Functions/1.ts
+++ Functions/1.ts
@@ -4,3 +4,49 @@ function greet(name:string){
     console.log(`hello ${name}`);
 }
 greet("Lovjeet singh")
+
+const names =["alice","bob","jack"];
+
+names.forEach((s)=>{
+    console.log(s);
+})
+
+function Cordi(pt:{x:number;y:number}){
+    console.log(`x cordinate is ${pt.x} and y cordinate is ${pt.y}`);
+}
+
+Cordi({x:7,y:8});
+
+//Type alias
+type Point={
+    x:number;
+    y:number;
+}
+function Cordii(pt:Point){
+    console.log(`x cordinate is ${pt.x} and y cordinate is ${pt.y}`);
+}
+
+Cordii({x:7,y:8});
+interface Teacher {
+    name: string;
+    course: string;
+}
+
+interface Student extends Teacher {
+    marks: number;
+}
+
+// Defining the `getStudent` function
+function getStudent(): Student {
+    return {
+        name: "John Doe",
+        course: "Mathematics",
+        marks: 95
+    };
+}
+
+// Using the function
+const detail = getStudent();
+console.log(detail.name);
+
+//https://dog.ceo/api/breeds/image/random
